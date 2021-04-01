585a8030a480c88aaab0aaa0d483e20a23c9725a
Lenxs <48335567+Lenxs@users.noreply.github.com>
2021-04-01 15:17:35 +0200

    update login & register (avec api de mike) penser a changer d'api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },NativeStorage],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/interfaces/user-register.ts
@@ -0,0 +1,6 @@
+export interface UserRegister {
+    pseudo?:string;
+    email:string;
+    password:string;
+    confirm_password:string;
+}
